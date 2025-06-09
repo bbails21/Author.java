dd5b68693d3f40f38ae20aadf5ed467b1bd6e92e
bbails21 <baileyortiz1999@gmail.com>
2025-06-09 02:36:24 +0000

    Main.java lab4

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Author author = new Author("J.K. Rowling", "jk.rowling@example.com", "British");
+        Book book = new Book("Harry Potter and the Philosopher's Stone", 19.99, author);
+
+        System.out.println(book.toString());
+    }
+}
