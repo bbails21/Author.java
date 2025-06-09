4e78b1ab6aebd91fb944f3617a84b164589b191b
bbails21 <baileyortiz1999@gmail.com>
2025-06-09 02:25:25 +0000

    Auhor.java Lab 4

--- Author.java
+++ Author.java
@@ -1,1 +1,31 @@
+public class Author {
+    private String name;
+    private String email;
+    private String nationality;
 
+    // Constructor
+    public Author(String name, String email, String nationality) {
+        this.name = name;
+        this.email = email;
+        this.nationality = nationality;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Author: " + name + ", Email: " + email + ", Nationality: " + nationality;
+    }
+}
